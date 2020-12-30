84e9c74040a9a9a49947870ad7df605308553953
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-12-30 20:44:10 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+function Card({ isDisplay, setIsDisplay }) {
+  return (
+    <div className="card">
+      <button onClick={() => setIsDisplay(!isDisplay)} className="btn">
+        點我
+      </button>
+    </div>
+  );
+}
+
+function PopUp({ isDisplay }) {
+  let className;
+  if (isDisplay) {
+    className = "show";
+  } else {
+    className = "hidden";
+  }
+  return (
+    <div className={`pop-up ${className}`}>
+      測試用測試用測試用測試用測試用測試用 測試用測試用測試用測試用測試用測試用
+      測試用測試用測試用測試用測試用測試用 測試用測試用測試用測試用測試用測試用
+      測試用測試用測試用測試用測試用測試用
+    </div>
+  );
+}
 
 function App() {
+  const [isDisplay, setIsDisplay] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card isDisplay={isDisplay} setIsDisplay={setIsDisplay} />
+      <PopUp isDisplay={isDisplay} />
     </div>
   );
 }
